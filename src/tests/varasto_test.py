8969eeb113fab91f6fc83e83f76f41d2a47c34ff
Peketeus <p3keteus@gmail.com>
2025-10-31 14:04:08 +0200

    Tehtävä 8 Unittest

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -5,6 +5,44 @@ from varasto import Varasto
 class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
+        self.virhevarasto = Varasto(-10, -5) # väärillä arvoilla luotu varasto
+        self.fullvarasto = Varasto(10, 11) # saldo suurempi kuin tilavuus
+
+# omat testit ---------------------------------------------------------------------------------
+
+    def test_konstruktori_luo_virheellisen_varaston_tilavuuden(self):
+        self.assertAlmostEqual(self.virhevarasto.tilavuus, 0)
+
+    def test_konstruktori_luo_virheellisen_varaston_saldon(self):
+        self.assertAlmostEqual(self.virhevarasto.saldo, 0)
+
+    def test_konstruktori_luo_liian_tayden_varaston(self):
+        self.assertAlmostEqual(self.fullvarasto.saldo, 10)
+
+    def test_lisays_lisaa_negatiivinen_saldo(self):
+        self.varasto.lisaa_varastoon(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisays_lisaa_varastoon_enemman(self):
+        self.varasto.lisaa_varastoon(11)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ottaminen_negatiivinen_arvo(self):
+        self.fullvarasto.ota_varastosta(-1)
+
+        self.assertAlmostEqual(self.fullvarasto.saldo, 10)
+
+    def test_ottaminen_saldoa_suurempi(self):
+        self.fullvarasto.ota_varastosta(11)
+
+        self.assertAlmostEqual(self.fullvarasto.saldo, 0)
+
+    def test_paljonko_tilaa(self):
+        self.assertEqual(str(self.fullvarasto), "saldo = 10, vielä tilaa 0")
+
+# omien testien loppu -------------------------------------------------------------------------
 
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
